b33ffc1c2a1a640c5428c31d620c0df73cc5f4e5
saytama8 <147938257+saytama8@users.noreply.github.com>
2024-09-05 17:52:38 +0300

    Add files via upload

--- phone.py
+++ phone.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv('manager.csv')
-df.index = [1,2,3,4]
-df['model'].plot(kind = 'hist')
-
+#df.index = [1,2,3,4]
+#df['Samsung','Apple','Huawei','Xiaomi']
+df["price"].plot(kind="pie")
 plt.show()
